a294a9e12c86f6dc75d3bcc38dc13382c7f118cd
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-07-25 14:01:40 +0900

    megaphone

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -3,10 +3,7 @@
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-		return (0);
-	}
 	for (int i = 1; i < argc; i++)
 	{
 		for (int j = 0; argv[i][j]; j++)
