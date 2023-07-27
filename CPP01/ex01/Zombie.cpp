d194adda035e036b340ed5628a4df24eabab5c9e
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-07-27 11:57:15 +0900

    remove unnecessary this

--- CPP01/ex01/Zombie.cpp
+++ CPP01/ex01/Zombie.cpp
@@ -1,14 +1,14 @@
 # include "Zombie.hpp"
 
 Zombie::~Zombie() {
-    std::cout << BLACK << this->m_name << ZOMBIE_DEAD << std::endl;
+    std::cout << BLACK << m_name << ZOMBIE_DEAD << std::endl;
 }
 
 void Zombie::announce() const {
-    std::cout << RED << this->m_name << ZOMBIE_BRAINS << DEFAULT << std::endl;
+    std::cout << RED << m_name << ZOMBIE_BRAINS << DEFAULT << std::endl;
 }
 
 void Zombie::SetName(std::string name) {
-    this->m_name = name;
-    std::cout << GREEN << this->m_name << ZOMBIE_BORN << GREEN << std::endl;
+    m_name = name;
+    std::cout << GREEN << m_name << ZOMBIE_BORN << GREEN << std::endl;
 }
