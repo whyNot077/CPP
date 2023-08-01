1b127a1bacd074a4fdeb5608690d0f6d4ad6f0f1
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-08-01 11:45:50 +0900

    use static cast

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -9,7 +9,7 @@ int	main(int argc, char **argv)
 	for (int i = 1; i < argc; i++)
 	{
 		for (int j = 0; argv[i][j]; j++)
-			std::cout << (char)std::toupper(argv[i][j]);
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
 	}
 	std::cout << std::endl;
 	return (0);
