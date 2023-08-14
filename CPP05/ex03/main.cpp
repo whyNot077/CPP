dbbac02a46110c3ea1f80b38d8d8baa9b7485685
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-08-14 14:59:35 +0900

    change main function

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -8,8 +8,12 @@
 int main()
 {
     Intern intern;
+    AForm* robot;
     try {
-        intern.makeForm("robotomy request", "show me the money");
+        robot = intern.makeForm("robotomy request", "show me the money");
+        Bureaucrat Conan("Conan", 1);
+        Conan.signForm(*robot);
+        Conan.executeForm(*robot);
         intern.makeForm("presidential pardon", "power overwelming");
         intern.makeForm("shrubbery creation", "black sheep wall");
         intern.makeForm("unknown form", "unknown target");
