8d6579dc0e13cc4b9e4b7ecf2abfbea2e7faf045
Minju Kim <peppertones0123@gmail.com>
2023-10-08 14:59:46 +0900

    btc

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -112,11 +112,6 @@ bool BitcoinExchange::IsValidDate(const std::string& date) const {
         std::cout << "Error: bad input => " << date << std::endl;
         return false;
     }
-    if (month < 1 || month > 12) {
-        std::cout << "Error: bad input => " << date << std::endl;
-        return false;
-    }
-
     switch (month) {
         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
             if (day < 1 || day > 31) {
